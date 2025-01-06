6931f74b2fd89885191153e3481072425a4e1751
leoge <gyhaoran@gmail.com>
2025-01-06 17:13:16 +0800

    add setup py

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # r-tools
-# r-tools
+> EDA Tools based on [Rsync-x](https://github.com/RsynTeam/rsyn-x)
--- python/example.py
+++ python/example.py
@@ -3,5 +3,5 @@ import python
 
 
 if __name__ == '__main__':
-    input = '{"expand":true,"lefFiles":"pac.lef","min_space":0.06,"min_width":0.06,"path":"/home/ranhaooge/code/cpp/rsyn-x/x/demo/simple","score_file":"pac.json"}'
+    input = '{"expand":true,"lefFiles":"pac.lef","min_space":0.06,"min_width":0.06,"path":"/home/ranhaooge/code/cpp/rsyn-x/x/demo/simple"}'
     
